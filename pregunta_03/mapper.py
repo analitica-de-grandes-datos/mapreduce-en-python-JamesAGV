4000579ba1e8f69883b4e7a139d1eca0a38c611f
JamesAGV <102199710+JamesAGV@users.noreply.github.com>
2022-06-09 12:20:27 -0500

    pregunta_03
    solución

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+   for line in sys.stdin:
+      lista=line.split(',')
+      letter=lista[0]
+      value=lista[1]
+      sys.stdout.write("{}\t{}".format(letter, value))
--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,25 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+import numpy as np
+
+if __name__ == '__main__':
+
+    letters=[]
+    values=[]
+
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = int(val)
+        letters.append(key)
+        values.append(val)
+        # sys.stdout.write("{}\n".format(lista))
+    
+    letters=np.array(letters)
+    values=np.array(values)
+    ordered=np.argsort(values)
+
+    for i in ordered:
+        sys.stdout.write("{}\t{}\n".format(letters[i],values[i]))
