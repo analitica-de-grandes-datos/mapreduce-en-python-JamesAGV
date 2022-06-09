9e4acd819d226bef69548a542fdf173eadd6052f
JamesAGV <102199710+JamesAGV@users.noreply.github.com>
2022-06-09 12:43:23 -0500

    update
    corrección

--- pregunta_03/test.py
+++ pregunta_03/test.py
@@ -11,13 +11,15 @@ c,2
 a,3
 e,4
 f,5
-b,6
-""".split(
+b,6""".split(
     "\n"
 )
+
 
 if len(lines) != len(expected):
     raise Exception("Wrong number of lines")
 
 for solution, expected in zip(lines, expected):
     assert solution == expected, f"Expected: {expected}\nGot: {solution}"
+
+
