d91822eba65b33ed53b253e6693fd17d86ede156
JamesAGV <102199710+JamesAGV@users.noreply.github.com>
2022-06-09 06:12:09 -0500

    map
    solución

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,13 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+   n=0
+   for line in sys.stdin:
+      if n>0:
+         lista=line.split(',')
+         purpose=lista[3]
+         amount=lista[4]
+         sys.stdout.write("{}\t{}\n".format(purpose, amount))
+      n=1
