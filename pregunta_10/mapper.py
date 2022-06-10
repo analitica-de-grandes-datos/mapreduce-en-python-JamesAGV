641ecfe9a33db36649c422f89235678380774152
JamesAGV <102199710+JamesAGV@users.noreply.github.com>
2022-06-09 22:33:52 -0500

    pregunta_10
    solución

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,13 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+
+   for line in sys.stdin:
+      number, letters = line.replace('\n','').split('\t')
+      #sys.stdout.write("{}\t{}".format(number, letters))
+      for letter in letters.split(','):
+        sys.stdout.write("{}\t{}\n".format(letter, number))
+        #tupla=(number,letter)
+        #rite("{}\n".format(tupla))
--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,44 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+    curkey = None
+    lista=[]
+
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        key=key.strip()
+        val=int(val)
+        #sys.stdout.write("{}\n".format(len(key)))
+
+        if key == curkey:
+            #
+            # No se ha cambiado de clave. Aca se acumulan los valores para la misma
+            # clave.
+            #
+            lista.append(val)
+        else:
+            #
+            # Se cambio de clave. Se reinicia el acumulador.
+            #
+            if curkey is not None:
+                #
+                # una vez se han reducido todos los elementos
+                # con la misma clave se imprime el resultado en
+                # el flujo de salida
+                #
+                sys.stdout.write("{} {}\n".format(curkey, ','.join(str(i) for i in sorted(lista))))
+                
+                lista=[]
+
+            curkey = key
+            lista.append(val)
+
+    sys.stdout.write("{} {}\n".format(curkey, ','.join(str(i) for i in sorted(lista))))
